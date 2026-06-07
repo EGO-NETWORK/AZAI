cf4273327cc8ce0bf67bc81589d4ed8e4b07696f
EGO-NETWORK <royalnobi768@gmail.com>
2026-06-08 04:45:54 +0530

    Prepare AZAI group gate imports

--- AloneX/plugins/test_gate_note.py
+++ AloneX/plugins/test_gate_note.py
@@ -1,1 +1,12 @@
-# AZAI gate note
+import random
+from datetime import datetime
+
+import pytz
+from telethon import Button, events
+
+from AloneX import database, font, prefix_cmds, tbot
+
+IST = pytz.timezone("Asia/Kolkata")
+AZAI_BOT_USERNAME = "Urxazaibot"
+gate_db = database["azai_group_gate"]
+active_gate = {}
