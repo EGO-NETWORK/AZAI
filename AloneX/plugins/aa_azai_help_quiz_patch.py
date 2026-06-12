aab64efd2b2eea4a7eb0626982568d682a9da317
EGO-NETWORK <royalnobi768@gmail.com>
2026-06-13 02:28:07 +0530

    Rename quiz help button

--- AloneX/plugins/aa_azai_help_quiz_patch.py
+++ AloneX/plugins/aa_azai_help_quiz_patch.py
@@ -9,7 +9,7 @@ def hb():
         [Button.inline(font("Core"), b"azai_help_core"), Button.inline(font("Owner"), b"azai_help_owner")],
         [Button.inline(font("Economy"), b"azai_help_economy"), Button.inline(font("Market"), b"azai_help_market")],
         [Button.inline(font("Family"), b"azai_help_family"), Button.inline(font("Games"), b"azai_help_games")],
-        [Button.inline(font("Quiz"), b"azai_help_quiz"), Button.inline(font("Media"), b"azai_help_media")],
+        [Button.inline(font("Anime Quiz"), b"azai_help_quiz"), Button.inline(font("Media"), b"azai_help_media")],
         [Button.inline(font("System"), b"azai_system_stats"), Button.inline(font("Back"), b"azai_start_home")],
         [Button.inline(font("Close"), b"azai_close_panel")],
     ]
@@ -20,7 +20,7 @@ def ht():
 
 
 def qt():
-    return font("QUIZ COMMANDS") + "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n" + "/animeguess\n/quiz\n/quizstats\n/quiztop"
+    return font("ANIME QUIZ COMMANDS") + "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n" + "/animeguess\n/quiz\n/quizstats\n/quiztop"
 
 
 p.help_buttons = hb
