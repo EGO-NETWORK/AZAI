138e9b87b044a41791867e808e380c0acfee623b
EGO-NETWORK <royalnobi768@gmail.com>
2026-06-21 21:34:34 +0530

    fix: add azai economy shim

---
+++ AloneX/plugins/azai_economy.py
@@ -0,0 +1,6 @@
+from AloneX.plugins.zzzz_azai_ego_hustle_core import daily as daily_handler
+from AloneX.plugins.zzzz_azai_ego_hustle_core import bal as wallet_handler
+from AloneX.plugins.zzzz_azai_ego_hustle_core import top as leaderboard_handler
+
+async def inventory_handler(event):
+    return None
